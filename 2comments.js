0c3e7fb514c86d77763ab2d80602862267eecaa5
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-01-06 15:09:57 -0300

    Code comments

---
+++ 2comments.js
@@ -0,0 +1,8 @@
+// Comentários em JavaScript é uma parte do código que não é executada.
+// Existem comentários de linha, como é o caso deste comentário.
+console.log("Os comentários são ignorados pela engine do javascript");
+/* 
+Existe também os comentários em bloco. Tudo o que está dentro desse bloco
+não será executado
+Comentários são úteis para escrever alguma informação referente ao código.
+*/
