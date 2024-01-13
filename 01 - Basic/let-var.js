812b6ee4a7d0c0109a454666753a2259cb36d17a
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-01-13 16:11:03 -0300

    Let vs Var - First difference

---
+++ 01 - Basic/let-var.js
@@ -0,0 +1,12 @@
+/* 
+Diferenças entre var e let:
+1. Com var nós podemos redeclarar uma variável
+2.
+
+ECMAScript = Padronização do JavaScript
+*/
+// 1
+var nome = "Maria";
+var nome = "Fernanda";
+
+nome = "Texto aqui" // NÃO CRIE VARIÁVEIS SEM CONST E SEM LET
