48f1474d6ea8423f3324839a39b34d1f60276940
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-01-22 19:57:20 -0300

    Alert, confirm, prompt

---
+++ 01 - Basic/alert-confirm-prompt/index.js
@@ -0,0 +1,4 @@
+const number1 = parseFloat(prompt("Digite o primeiro número"));
+const number2 = parseFloat(prompt("Digite o segundo número"));
+const result = number1 + number2;
+alert(`O resultado da sua conta foi: ${result}`);
