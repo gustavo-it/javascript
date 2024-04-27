59c3842ed25675abbb08b028544dc3a45c049e5c
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-04-27 14:40:28 -0300

    Comparison Operatos

---
+++ 02 - Logic/1comparison-operators.js
@@ -0,0 +1,13 @@
+/* 
+Operadores de comparação
+> - maior que
+>= - maior ou igual a
+< - menor que
+<= - menor que ou igual a
+== - igualdade (Não é recomendado)
+=== - igualdade e compara o tipo do dado
+!= - diferente de (Não é recomendado)
+!== - diferente de e compara o tipo de dado
+
+Todos esses operadores retornam o valor True ou False.
+*/
