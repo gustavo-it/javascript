10bea6195003e786f793c6af07d96ec32f861f3e
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-04-28 12:44:53 -0300

    Final Solution

--- 02 - Logic/imc-calculation/assets/js/index.js
+++ 02 - Logic/imc-calculation/assets/js/index.js
@@ -1,84 +1,67 @@
-/* 
-Próximo passo : 
-Criar uma função que vai receber a resposta do setMessage, a mensagem a ser exebida, e vai adicionar essa mensagem no html.
-*/
+const form = document.querySelector("#form");
 
-const form = document.getElementById("form");
-const answers = [
-    "Abaixo do peso", "Peso normal", "Sobrepeso",
-    "Obesidade grau 1", "Obesidade grau 2", "Obesidade grau 3",
-    "Altura inválida", "Peso inválido"
-]
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const inputWeight = event.target.querySelector("#weight");
+    const inputHeight = event.target.querySelector("#height");
 
-const getElements = () => {
-    const weight = document.getElementById("weight");
-    const height = document.getElementById("height");
+    const weight = Number(inputWeight.value);
+    const height = Number(inputHeight.value) / 100;
 
-    return { weight, height };
+    if (!weight) {
+        setResult("Peso inválido", false);
+        return;
+    };
+
+    if (!height) {
+        setResult("Altura inválida", false);
+        return;
+    };
+
+    const imc = getImc(weight, height);
+    const levelImc = getLevelImc(imc);
+    const msg = `Seu IMC é ${imc} ${levelImc}`;
+
+    setResult(msg, true);
+});
+
+const getLevelImc = imc => {
+    const level = [
+        "Abaixo do peso", "Peso normal", "Sobrepeso",
+        "Obesidade grau 1", "Obesidade grau 2", "Obesidade grau 3"
+];
+
+    if (imc >= 39.9) return level[5];
+    if (imc >= 34.9) return level[4];
+    if (imc >= 29.9) return level[3];
+    if (imc >= 24.9) return level[2];
+    if (imc >= 18.5) return level[1];
+    if (imc < 18.5) return level[0];
 };
 
-const elements = getElements();
-
-const formDataProcessed = (object) => {
-    const weight = Number.parseInt(object.weight.value);
-    const height = parseFloat(object.height.value) / 100;
-    if (isNaN(height)) {
-        document.getElementById("result").innerHTML = `<p>${answers[6]}</p>`;
-    } else if (isNaN(weight)) {
-        document.getElementById("result").innerHTML = `<p>${answers[7]}</p>`;
-    }
-    return { weight, height }
-};
-
-const imcCalc = (object) => {
-    const weight = object.weight;
-    const height = object.height;
-    const imc = weight / (height * height);
-    console.log(imc);
+const getImc = (weight, height) => {
+    const imc = weight / height ** 2;
+    console.log(imc)
     return imc.toFixed(2);
 };
 
-const setMessage = (resultImc) => {
-
-    const div = document.getElementById("result");
-
-    if (resultImc < 18.5){
-        console.log(answers[0])
-        clearValues(elements);
-        document.getElementById("result").classList.add("bad")
-        return div.innerHTML = `<p>Seu imc é: ${resultImc} (${answers[0]})</p>`;
-    }
-     else if (resultImc >= 18.5 && resultImc <= 24.9) {
-        console.log(answers[1])
-        clearValues(elements);
-        return div.innerHTML = `<p>Seu imc é: ${resultImc} (${answers[1]})</p>`
-    } else if (resultImc >= 25 && resultImc <= 29.9) {
-        console.log(answers[2]);
-        return div.innerHTML = `<p>Seu imc é: ${resultImc} (${answers[2]})</p>`
-    } else if (resultImc >= 30 && resultImc <= 34.9) {
-        console.log(answers[3]);
-        return div.innerHTML = `<p>Seu imc é: ${resultImc} (${answers[3]})</p>`
-    } else if (resultImc >= 35 && resultImc <= 39.9) {
-        console.log(answers[4]);
-        return div.innerHTML = `<p>Seu imc é: ${resultImc} (${answers[4]})</p>`
-    } else if (resultImc > 40) {
-        console.log(answers[5]);
-        return div.innerHTML = `<p>Seu imc é: ${resultImc} (${answers[5]})</p>`
-    }
-}
-
-const clearValues = elements => {
-    elements.weight.value = '';
-    elements.height.value = '';
+const createP = () => {
+    const p = document.createElement("p");
+    return p;
 };
 
-const preventDefault = element => {
-    element.addEventListener("submit", event => {
-        event.preventDefault();
-        const data = formDataProcessed(elements);
-        const imc = imcCalc(data);
-        setMessage(imc);
-    });
-};
+const setResult = (msg, isValid) => {
+    const result = document.querySelector("#result");
+    result.innerHTML = "";
 
-preventDefault(form);
+    const p = createP();
+
+    if (isValid) {
+        p.classList.add("paragraph-result");
+    } else {
+        p.classList.add("bad");
+    }
+
+    p.innerHTML = msg;
+    result.appendChild(p);
+};
