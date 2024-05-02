a38aafac9f5051d870bf421ce018bc75328d703b
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-05-02 10:28:49 -0300

    Programming Logic - 01

---
+++ 02 - Logic/15logic-programming-01.js
@@ -0,0 +1,4 @@
+// Escreve uma função que recebe 2 números e retorne o maior deles
+
+const returnsHigher = (number1, number2) => number1 > number2 ? number1 : number2;
+console.log(returnsHigher(419023, 29182123));
