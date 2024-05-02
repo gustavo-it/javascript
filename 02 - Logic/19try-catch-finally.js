7aac0d8b7104dca99bf696fa13a699769eab1110
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-05-02 13:08:10 -0300

    Treating and throwing mistakes (try, catch, finally)

---
+++ 02 - Logic/19try-catch-finally.js
@@ -0,0 +1,9 @@
+try {
+    console.log("Abri um arquivo");
+    console.log("Manipulei o arquivo");
+    console.log("Fechei o arquivo");
+} catch (error) {
+    console.log("Tratando o error")
+} finally {
+    console.log("Sempre sou executado.")
+}
