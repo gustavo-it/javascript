3b8aa8ecad17fa36d8075bde89bcf6f2aaa75670
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-05-03 11:19:13 -0300

    Timer functionality

--- 02 - Logic/timer-setInterval/assets/js/index.js
+++ 02 - Logic/timer-setInterval/assets/js/index.js
@@ -1,26 +1,44 @@
-const watch = document.querySelector(".watch");
-const start = document.querySelector(".start");
-const stop = document.querySelector(".stop");
-const reset = document.querySelector(".reset");
-
-start.addEventListener("click", event => {
-    alert("Botão clicado");
-});
-
-stop.addEventListener("click", event => {
-    alert("Botão clicado");
-});
-
-reset.addEventListener("click", event => {
-    alert("Botão clicado");
-});
-
-const returnHours = (segundos) => {
-    const hours = new Date(segundos * 1000);
-    return hours.toTimeString('pt-BR', {
-        hours12: false,
-        timeZone: "UTC",
+function relogio() {
+    function criaHoraDosSegundos(segundos) { // Recebe os segundos, multiplica por mil e retorna.
+      const data = new Date(segundos * 1000);
+      return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+      });
+    }
+    const relogio = document.querySelector('.watch');
+    let segundos = 0;
+    let timer;
+  
+    function iniciaRelogio() {
+    // Inicia o contador, incrementando a cada chamada da função (que acontece a cada 1 segundo)
+      timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+      }, 1000);
+    }
+  
+    document.addEventListener('click', function(e) {
+      const el = e.target; // Pega todo elemento que está sendo clicado
+      console.log(e.target)
+  
+      if (el.classList.contains('reset')) { // Zera o relógio
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        relogio.classList.remove('pausado');
+        segundos = 0;
+      }
+  
+      if (el.classList.contains('start')) { // Inicia o relógio
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+      }
+  
+      if (el.classList.contains('stop')) { // Para o relógio
+        clearInterval(timer);
+        relogio.classList.add('pausado');
+      }
     });
-}
-
-const data = returnHours(0);
+  }
+relogio();
