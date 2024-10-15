a516fc815f8f1c9bb97fbcdbbe7008c03b8a574f
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-10-15 13:36:14 -0300

    Async/Await update code

--- 07 - Asynchronous/03async-await.js
+++ 07 - Asynchronous/03async-await.js
@@ -1,18 +1,45 @@
-function wait (message, time) {
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (typeof message !== "string") reject("BAD VALUE");
-
-            resolve(message + " Passei na promise");
-        }, time);
-    })
-
-}
-
-const random = (min, max) => {
+function rand(min = 0, max = 3) {
     min *= 1000;
     max *= 1000;
-    return Math.floor(Math.random() * (max - min) + min)
-};
-
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(typeof msg !== 'string') {
+          reject('CAI NO ERRO');
+          return;
+        }
+  
+        resolve(msg.toUpperCase() + ' - Passei na promise');
+        return;
+      }, tempo);
+    });
+  }
+  
+  
+  async function executa() {
+    try {
+      const fase1 = await esperaAi('Fase 1', 1000);
+      console.log(fase1);
+  
+      setTimeout(function() {
+        console.log('Essa promise estava pendente', fase1);
+      }, 1100);
+  
+      const fase2 = await esperaAi('Fase 2', rand());
+      console.log(fase2);
+  
+      const fase3 = await esperaAi('Fase 3', rand());
+      console.log(fase3);
+  
+      console.log('Terminamos na fase:', fase3);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+  // executa();
+  
+  const teste2 = esperaAi('qlq', 5000);
+  console.log(teste2);
