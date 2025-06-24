5fe405abd8c322f2e28d68af679f055ba8db019a
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-24 13:01:13 -0300

    Selecting and Manipulating Elements

--- javascript-dom/guess-my-number/script.js
+++ javascript-dom/guess-my-number/script.js
@@ -1,1 +1,7 @@
-const message = document.querySelector(".message").textContent;
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!";
+document.querySelector(".number").textContent = 13; // número aleatório gerado
+document.querySelector(".score") .textContent = 20; // score do usuário
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value) // selecionando o input onde o usuário digita as informações
