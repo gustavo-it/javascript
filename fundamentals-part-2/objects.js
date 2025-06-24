f00e395425b4e4cc23cfaa2f19368ab43abacd65
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-24 10:59:11 -0300

    Introduction to Objects

---
+++ fundamentals-part-2/objects.js
@@ -0,0 +1,13 @@
+const person = {
+    firstName: "Maria",
+    lastName: "Barbosa",
+    age: 24
+}
+
+// Acessando os valores do array
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+
+// Acessando valores com colchetes
+console.log(person['firstName']);
