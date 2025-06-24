5ef328c3521c217729c730b423d85a8d2f553cbf
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-24 10:27:19 -0300

    Logical Operators

---
+++ fundamentals-part-1/logical-operators.js
@@ -0,0 +1,8 @@
+const testA = true;
+const testB = true;
+const testC = false;
+
+console.log(testA && testB); // true
+console.log(testA && testC); // false
+console.log(testA || testC); // false
+console.log(!testC);
