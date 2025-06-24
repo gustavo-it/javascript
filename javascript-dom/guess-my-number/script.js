75fef02e962c0275ee9e07c603606432e67ae6d9
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-24 15:16:41 -0300

    Implementing the Game Logic

--- javascript-dom/guess-my-number/script.js
+++ javascript-dom/guess-my-number/script.js
@@ -1,14 +1,39 @@
 const buttonCheck = document.querySelector(".check");
 const inputGuess = document.querySelector(".guess");
 const message = document.querySelector(".message");
+const number = document.querySelector(".number");
+let score = 20;
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+number.textContent = secretNumber;
+
+const setTextContent = (element, message) => element.textContent = message;
 
 buttonCheck.addEventListener("click", event => {
     const guess = Number(inputGuess.value);
-    console.log(guess);
-    console.log(typeof guess);
-    message.textContent = "Correct Number!";
+    const divScore = document.querySelector(".score");
 
     if (!guess) {
-        message.textContent = "No number!";
+        setTextContent(message, "No number!");
+    } else if (guess === secretNumber) { //
+        setTextContent(message, "Correct number!");
+    } else if (guess > secretNumber) {
+        if(score > 0) {
+            setTextContent(message, "Too high!");
+            score--;
+            setTextContent(divScore, score);
+    } else {
+        setTextContent(message, "You lost the game!");
+        setTextContent(divScore, 0);
+    }
+    } else if (guess < secretNumber) {
+        if (score > 0) {
+            setTextContent(message, "Too low!");
+            score--;
+            setTextContent(divScore, score);
+        } else {
+            setTextContent(message, "You lost the game!");
+            setTextContent(divScore, 0);
+        }
     }
 });
