fc55271fa8bd7184edde491dcad01325fb4a4f99
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-24 10:31:28 -0300

    The Switch Statement

---
+++ fundamentals-part-1/switch.js
@@ -0,0 +1,7 @@
+const day = 2;
+
+switch(day) { // Valor a ser comparado
+    case 1: console.log("É Segunda-feira"); break;
+    case 2: console.log("É Terça-feira"); break; // Break é responsável por finalizar o código assim que ele encontrar o case correspondente.
+    default: console.log("Não identificamos o dia"); // É chamado quando nenhum dos case corresponde ao valor.
+}
