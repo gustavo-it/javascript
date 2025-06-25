e45d7d3105dc37fce6e5eaa98e5be21fa15f4a6d
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-24 22:29:55 -0300

    Refactoring the code

--- javascript-dom/guess-my-number/script.js
+++ javascript-dom/guess-my-number/script.js
@@ -4,38 +4,29 @@ const message = document.querySelector(".message");
 const number = document.querySelector(".number");
 let score = 20;
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 number.textContent = secretNumber;
 
-const setTextContent = (element, message) => element.textContent = message;
+const setTextContent = (element, message) => (element.textContent = message);
 
-buttonCheck.addEventListener("click", event => {
-    const guess = Number(inputGuess.value);
-    const divScore = document.querySelector(".score");
+buttonCheck.addEventListener("click", (event) => {
+  const guess = Number(inputGuess.value);
+  const divScore = document.querySelector(".score");
 
-    if (!guess) {
-        setTextContent(message, "No number!");
-    } else if (guess === secretNumber) { //
-        setTextContent(message, "Correct number!");
-        document.querySelector("body").style.backgroundColor = "#60b347"; // sempre utilizar string
-        document.querySelector(".number").style.width = "30rem";
-    } else if (guess > secretNumber) {
-        if(score > 0) {
-            setTextContent(message, "Too high!");
-            score--;
-            setTextContent(divScore, score);
+  if (!guess) setTextContent(message, "No number!");
+
+  if (guess === secretNumber) {
+    setTextContent(message, "Correct number!");
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+  } else {
+    if (score > 0) {
+      setTextContent(message, guess > secretNumber ? "Too high!" : "Too low!");
+      score--;
+      setTextContent(divScore, score);
     } else {
-        setTextContent(message, "You lost the game!");
-        setTextContent(divScore, 0);
+      setTextContent(message, "You lost the game!");
+      setTextContent(divScore, 0);
     }
-    } else if (guess < secretNumber) {
-        if (score > 0) {
-            setTextContent(message, "Too low!");
-            score--;
-            setTextContent(divScore, score);
-        } else {
-            setTextContent(message, "You lost the game!");
-            setTextContent(divScore, 0);
-        }
-    }
+  }
 });
