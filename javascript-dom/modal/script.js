57ba0b9a6d1e8412ec0c4bb4a71dfa6894866e04
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-25 11:46:17 -0300

    Working With Classes

--- javascript-dom/modal/script.js
+++ javascript-dom/modal/script.js
@@ -2,4 +2,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModel = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+const openModal = () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);
+};
+
+const closeModel = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+btnCloseModel.addEventListener("click", closeModel);
+
+overlay.addEventListener("click", closeModel);
