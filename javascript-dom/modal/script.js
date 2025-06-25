32dd846d88b368afe23371a1057f99ab834c2517
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-25 10:54:23 -0300

    Modal project config

---
+++ javascript-dom/modal/script.js
@@ -0,0 +1,5 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
