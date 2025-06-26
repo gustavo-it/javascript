31a7061a30873622417daeced219e54711ceb98e
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-26 14:37:27 -0300

    Styling for class hidden and selecting first elements

---
+++ javascript-dom/pig-game/script.js
@@ -0,0 +1,9 @@
+
+// Selecting elements
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
