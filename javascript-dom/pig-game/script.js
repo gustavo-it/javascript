0a24d008aa21654fba5a3e413eac9cfdddce7a43
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-26 16:14:06 -0300

    Switching the Active Player

--- javascript-dom/pig-game/script.js
+++ javascript-dom/pig-game/script.js
@@ -1,5 +1,3 @@
-
-// Selecting elements
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -10,7 +8,12 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+const secPlayer0 = document.querySelector(".player--0");
+const sectPlayer1 = document.querySelector(".player--1");
+
+const scores = [];
 let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -24,6 +27,12 @@ btnRoll.addEventListener("click", event => {
 
     if(dice !== 1) {
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        secPlayer0.classList.toggle("player--active");
+        sectPlayer1.classList.toggle("player--active");
     }
 })
