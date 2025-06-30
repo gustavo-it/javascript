6b9ce949a364efdfadde6db32c870cad28e75e7f
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-06-30 15:56:49 -0300

    Rest Pattern and Parameters

---
+++ data-structures-modern-operators/rest-pattern-parameters.js
@@ -0,0 +1,14 @@
+// Rest operator
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [a, b, ...c] = numbers;
+console.log(c);
+
+// Rest operators em parâmetro de função
+const add = (...numbers) => {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum
+}
+console.log(add(1, 2, 3, 4, 5));
