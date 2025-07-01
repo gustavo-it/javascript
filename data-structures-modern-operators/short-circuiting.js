4f7b446ff8ec9fa27c0dfea9aab64828fe91a796
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-01 09:55:20 -0300

    Short Circuiting (&& and ||)

---
+++ data-structures-modern-operators/short-circuiting.js
@@ -0,0 +1,11 @@
+// Operador OR Retorna o primeiro valor verdadeiro encontrado
+// Ou retornar o último valor, caso todos eles sejam valores false
+console.log(3 || "Maria");
+console.log("" || "Maria");
+console.log(true || 0);
+console.log(undefined || null);
+
+// Operador AND Retorna o primeiro valor falso encontrado caso exista
+// Caso não exista retorna o último falor true encontrado
+console.log(0 && "Maria");
+console.log(7 && "Maria");
