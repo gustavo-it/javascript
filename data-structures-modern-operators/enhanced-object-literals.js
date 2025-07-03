9ac75c3ac372ed9d9b23fcf9e18beb7b22238de5
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-03 09:22:21 -0300

    Enhanced Object Literals

---
+++ data-structures-modern-operators/enhanced-object-literals.js
@@ -0,0 +1,19 @@
+const weekDays = ["mon", "tue", "wed", "sat"];
+
+const openingHours = {
+    [weekDays[3]]: {
+        open: 0,
+        close: 24,
+    },
+};
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours,
+
+  order(starterIndex, menuIndex) { return (starterMenu[menuIndex], starterMenu[starterIndex]) }
+};
