f245528c3bed25082a7e8c71515c253eefb49a34
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-08 11:03:54 -0300

    Working With Strings - Part 1

---
+++ data-structures-modern-operators/strings-part-1.js
@@ -0,0 +1,11 @@
+const airline = "TAP Air Portugal";
+
+// Retorna a primeira letra r encontrada
+console.log(airline.indexOf("r"));
+
+// Retorna a última letra r encontrada
+console.log(airline.lastIndexOf("r"));
+
+// Fatiar a string - Aceita valores negativos
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
